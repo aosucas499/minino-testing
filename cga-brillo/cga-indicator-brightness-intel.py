1d848047d8276c720da0ab4661b2a1fbef89f0ee
aosucas499 <aosucas499@gmail.com>
2023-06-12 20:04:43 +0000

    Add files via upload

--- cga-brillo/cga-indicator-brightness-intel.py
+++ cga-brillo/cga-indicator-brightness-intel.py
@@ -22,9 +22,9 @@ import gtk
 import appindicator
 import subprocess
 import re
-#import dbus
-#import dbus.service
-#from dbus.mainloop.glib import DBusGMainLoop
+import dbus
+import dbus.service
+from dbus.mainloop.glib import DBusGMainLoop
 
 #Granularidad del controlador
 grad = 10
@@ -62,31 +62,31 @@ def restaurar_brillo_cero():
 		ajustar_brillo("1")
 
 def obtener_brillo_actual():
-    with open('/sys/class/backlight/intel_backlight/brightness', 'r') as archivo:
+    with open('/sys/class/backlight/acpi_video0/brightness', 'r') as archivo:
         brillo_actual = int(archivo.read())
     return brillo_actual
 
 def obtener_max_brillo():
-    with open('/sys/class/backlight/intel_backlight/max_brightness', 'r') as archivo:
+    with open('/sys/class/backlight/acpi_video0/max_brightness', 'r') as archivo:
         max_brillo = int(archivo.read())
     return max_brillo
 
-def subir_brillo(porcentaje):
+def subir_brillo(grad):
     brillo_actual = obtener_brillo_actual()
     max_brillo = obtener_max_brillo()
-    nuevo_brillo = min(brillo_actual + int(max_brillo * porcentaje / 100), max_brillo)
+    nuevo_brillo = min(brillo_actual + int(max_brillo * int(grad) / 100), max_brillo)
     ajustar_brillo(nuevo_brillo)
 
-def bajar_brillo(porcentaje):
+def bajar_brillo(grad):
     brillo_actual = obtener_brillo_actual()
-    nuevo_brillo = max(brillo_actual - int(brillo_actual * porcentaje / 100), 0)
+    nuevo_brillo = max(brillo_actual - int(brillo_actual * int(grad) / 100), 0)
     ajustar_brillo(nuevo_brillo)
 
-def ajustar_brillo(porcentaje):
+def ajustar_brillo(grad):
     max_brillo = obtener_max_brillo()
-    nuevo_brillo = int(max_brillo * porcentaje / 100)
+    nuevo_brillo = int((max_brillo * int(grad)) / 10)
     nuevo_brillo = min(max(nuevo_brillo, 0), max_brillo)
-    with open('/sys/class/backlight/intel_backlight/brightness', 'w') as archivo:
+    with open('/sys/class/backlight/acpi_video0/brightness', 'w') as archivo:
         archivo.write(str(nuevo_brillo))
 
 def menu_item_response(mi, var):
